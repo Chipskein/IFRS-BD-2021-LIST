43d4318147b86515796703baec2c7ce4c248dee7
Chipskein <abfn0905@gmail.com>
2021-09-13 19:01:32 -0300

    salve

--- backend.php
+++ backend.php
@@ -1,3 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>backend</title>
+</head>
+<style>
+    body{
+        background-color: #454141;
+        color: white;
+    }
+</style>
+<body>
+<?php
+function calculardata($data,$fimdata){
+    $feriados=[];
+}
+?>
 <?php
     echo "<h3>Exercicio 1</h3>";
     echo "<br>";
@@ -6,16 +26,50 @@
         echo (preg_match("#^[0-9]{9}-[0-9]{2}$#",$cpf)) ?  "CPF ".$cpf." foi cadastrado com sucesso":"Seu cpf não foi cadastrado com sucesso";
     } else echo "CPF não foi enviado";
     echo "<br>";
+
+
+
+
+
     echo "<h3>Exercicio 2</h3>";
     if(isset($_POST["data"])&&isset($_POST["dias"])){
         $data=$_POST["data"];
         $dias=$_POST["dias"];
-        echo 'data:'.$data;
-        echo "<br>";
-        echo 'dias:'.$dias;
-        //echo (preg_match("#^[0-9]{9}-[0-9]{2}#",$cpf)) ?  "CPF ".$cpf." foi cadastrado com sucesso":"Seu cpf não foi cadastrado com sucesso";
+        if(preg_match("#^(0[1-9]|1[1-9]|2[1-9]|3[0-1])\/(0[1-9]|1[0-2])\/[0-9]*$#",$data)){
+            $format_data=substr($data,6)."/".substr($data,3,-5)."/".substr($data,0,2);
+            echo 'começo data:'.$data;
+            echo "<br>";
+            if(preg_match("#^[1-9]{1}[0-9]*$#",$dias)){
+                echo "dias uteis ".$dias;
+                calculardata(strtotime($format_data),strtotime($format_data ."+{$dias} days"));
+            }
+            else echo "dias inválidos";
+        }
+        else echo "Data inválida";    
     } else echo "a Data e os Dias uteis não foram enviados";
     echo "<br>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     echo "<h3>Exercicio 3</h3>";
     if(isset($_POST["valor"])){
         $valor=$_POST["valor"];
@@ -23,3 +77,5 @@
         //echo (preg_match("#^[0-9]{9}-[0-9]{2}#",$cpf)) ?  "CPF ".$cpf." foi cadastrado com sucesso":"Seu cpf não foi cadastrado com sucesso";
     } else echo "valor não foi enviado";
 ?>
+</body>
+</html>
