f368b04e792f5ed4320e7b22ae228aab519db027
Chipskein <abfn0905@gmail.com>
2021-09-13 12:44:09 -0300

    a)arrumado

--- backend.php
+++ backend.php
@@ -1,9 +1,13 @@
 <?php
-    if(isset($_POST["CPF"])){
+    if(isset($_POST["cpf"])){
         $cpf=$_POST["cpf"];
-        echo (preg_match("#^[0-9]{9}-[0-9]{2}#",$cpf)) ?  "CPF ".$cpf." foi cadastrado com sucesso":"Seu cpf não foi cadastrado com sucesso";
-    }
-    else{
-        echo "CPF não foi enviado";
+        echo (preg_match("#^[0-9]{9}-[0-9]{2}$#",$cpf)) ?  "CPF ".$cpf." foi cadastrado com sucesso":"Seu cpf não foi cadastrado com sucesso";
+    } else echo "CPF não foi enviado";
+    if(isset($_POST["data"])&&isset($_POST["dias"])){
+        $data=$_POST["data"];
+        $dias=$_POST["dias"];
+        echo 'data:'.$data;
+        echo 'dia:'.$data;
+        //echo (preg_match("#^[0-9]{9}-[0-9]{2}#",$cpf)) ?  "CPF ".$cpf." foi cadastrado com sucesso":"Seu cpf não foi cadastrado com sucesso";
     }
 ?>
