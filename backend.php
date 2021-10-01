ba15d48ff6c31c0c5bafebd11f80014f8567f572
SilvioGQ <silvioquintana1@hotmail.com>
2021-09-30 21:54:16 -0300

    upado

--- backend.php
+++ backend.php
@@ -378,6 +378,66 @@
         if ($day <= $day_qt) return true;
         else return false;
     };
+
+    function convertToNumber($name){
+        $numeros = array(
+            "um" => 1,
+            "dois" => 2,
+            "tres" => 3,
+            "quatro" => 4,
+            "cinco" => 5,
+            "seis" => 6,
+            "sete" => 7,
+            "oito" => 8,
+            "nove" => 9,
+            'dez' => 10,
+            'onze' => 11,
+            'doze' => 12,
+            'treze' => 13,
+            'quatorze' => 14,
+            'quinze' => 15,
+            'desseseis' => 16,
+            'dessesete' => 17,
+            'dezoito' => 18,
+            'dezenove' => 19,
+            "vinte" => 20,
+            "trinta" => 30,
+            "quarenta" => 40,
+            "cinquenta" => 50,
+            "sessenta" => 60,
+            "setenta" => 70,
+            "oitenta" => 80,
+            "noventa" => 90,
+            "cem" => 100,
+            "cento" => 100,
+            "duzentos" => 200,
+            "trezentos" => 300,
+            "quatrocentos" => 400,
+            "quinhentos" => 500,
+            "seiscentos" => 600,
+            "setecentos" => 700,
+            "oitocentos" => 800,
+            "novecentos" => 900,
+            "mil" => 1000,
+            "milhão" => 1000000,
+            "milhões" => 1000000
+        );
+        // $name = 'noventa milhõess';
+        $name = explode(" ",$name);
+        for($i=0;$i<count($name);$i++){
+            if($strcmp($name[$i],'e')==null){
+                if(strcmp($name[$i],'milhões') == 0 || strcmp($name[$i],'milhão') == 0){
+                    echo 'devia multiplicar';
+                     $value *= $numeros[$name[$i]];
+                }else{
+                    echo 'devia somar';
+                    $value += $numeros[$name[$i]];
+                }
+            }
+        }
+        echo $value;
+    }
+
     function converteToRoman($roman){
         $romans = array(
             'M' => 1000,
