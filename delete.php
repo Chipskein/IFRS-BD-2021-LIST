12285a8db42e8436a537c5592345ccb96813aa8d
Chipskein <abfn0905@gmail.com>
2021-10-20 21:34:20 -0300

    delete

--- delete.php
+++ delete.php
@@ -3,6 +3,10 @@
         $sabor=$_GET['codigo_s'];
         $db=new SQLite3('pizza.db');
         $db->query("delete from sabor where sabor.codigo=$sabor");
+        echo "Sabor Removido";
+    }
+    else{
+        echo "Erro retornando";
     }
     header( "refresh:1;url=index.php" );
     die();
--- edit.php
+++ edit.php
@@ -1,4 +1,9 @@
 <?php
-    echo $_GET['codigo_s'];
-    $db=new SQLite3('pizza.db');
+    if(isset($_GET['codigo_s'])){
+        $sabor=$_GET['codigo_s'];
+        $db=new SQLite3('pizza.db');
+    }
+    else{
+        echo "XIIIIII";
+    }
 ?>
