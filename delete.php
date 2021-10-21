70ddcf27b63305f9c73080e02031f0039378d5b6
Chipskein <abfn0905@gmail.com>
2021-10-20 21:36:00 -0300

    delete

--- delete.php
+++ delete.php
@@ -2,8 +2,9 @@
     if(isset($_GET['codigo_s'])){
         $sabor=$_GET['codigo_s'];
         $db=new SQLite3('pizza.db');
-        $db->query("delete from sabor where sabor.codigo=$sabor");
-        echo "Sabor Removido";
+        $result=$db->query("delete from sabor where sabor.codigo=$sabor");
+        if($result) echo "Sabor Removido";
+        else echo "Erro retornando";
     }
     else{
         echo "Erro retornando";
