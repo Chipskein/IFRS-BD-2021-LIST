3afc3b4d735203507d45b1c359360e2839be9573
Chipskein <abfn0905@gmail.com>
2021-10-20 21:32:39 -0300

    delete

--- delete.php
+++ delete.php
@@ -1,7 +1,10 @@
 <?php
-    $sabor=$_GET['codigo_s'];
-    $db=new SQLite3('pizza.db');
-    $db->query("delete from sabor where sabor.codigo=$sabor");
+    if(isset($_GET['codigo_s'])){
+        $sabor=$_GET['codigo_s'];
+        $db=new SQLite3('pizza.db');
+        $db->query("delete from sabor where sabor.codigo=$sabor");
+    }
     header( "refresh:1;url=index.php" );
-    die();   
+    die();
+       
 ?>
